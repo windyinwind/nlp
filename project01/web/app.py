4f305f6cd2dd396125692c7eefbbf779f6d7b87f
Wei Liang <weiliang@edanzgroup.com>
2019-09-14 20:33:55 +0900

    Set up static server for project01

--- project01/web/app.py
+++ project01/web/app.py
@@ -1,18 +1,27 @@
-from flask import Flask, escape, request
-from flask import jsonify
+from flask import Flask, escape, request, jsonify, render_template, send_from_directory
 import sys
+import os
 sys.path.append('./model/')
 from model import get_speeches
 
 app = Flask(__name__)
-
+staic_folder = './static'
 @app.route('/')
-def hello():
-    name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+@app.route('/dashboard.html')
+def index():
+    #return render_template('dashboard.html')
+    return send_from_directory(staic_folder, 'dashboard.html')
+
+@app.route('/user.html')
+def user():
+    #return render_template('dashboard.html')
+    return send_from_directory(staic_folder, 'user.html')
 
 @app.route('/speeches')
 def speech():
     speeches = get_speeches();
     print(speeches)
     return jsonify(speeches) 
+
+if __name__ == '__main__':
+    app.run()
